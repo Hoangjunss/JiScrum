d3c610e47dbe384d65c491be5d50efbd9d33ebd9
Hoangjunss <109050903+Hoangjunss@users.noreply.github.com>
2025-06-13 12:26:22 +0700

    first commit

---
+++ JiScrum/src/test/java/com/baconbao/JiScrum/JiScrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baconbao.JiScrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiScrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
