f0a38343e5381d1784a042daa64661f5deb61f24
NKhanh0908 <147810372+NKhanh0908@users.noreply.github.com>
2025-07-01 22:24:40 +0700

    feat(service): add method interface AccountService and MemberService
    +  Member getMemberEntityById(Integer id): get member entity by id member
    + Account getPrincipal(): get account signed

--- JiScrum/src/main/java/com/baconbao/JiScrum/service/AccountService.java
+++ JiScrum/src/main/java/com/baconbao/JiScrum/service/AccountService.java
@@ -4,9 +4,12 @@ import com.baconbao.JiScrum.dto.account.AccountCreateDTO;
 import com.baconbao.JiScrum.dto.account.AccountDTO;
 import com.baconbao.JiScrum.dto.account.AuthenticationDTO;
 import com.baconbao.JiScrum.dto.account.FormLoginDTO;
+import com.baconbao.JiScrum.model.Account;
 
 public interface AccountService {
     AuthenticationDTO signIn(FormLoginDTO formLoginDTO);
 
     AccountDTO create(AccountCreateDTO accountCreateDTO);
+
+    Account getPrincipal();
 }
--- JiScrum/src/main/java/com/baconbao/JiScrum/service/MemberService.java
+++ JiScrum/src/main/java/com/baconbao/JiScrum/service/MemberService.java
@@ -4,6 +4,7 @@ package com.baconbao.JiScrum.service;
 import com.baconbao.JiScrum.dto.member.MemberCreateDTO;
 import com.baconbao.JiScrum.dto.member.MemberDTO;
 import com.baconbao.JiScrum.dto.member.MemberUpdateDTO;
+import com.baconbao.JiScrum.model.Member;
 import org.apache.coyote.BadRequestException;
 
 
@@ -12,5 +13,6 @@ public interface MemberService {
     MemberDTO createMember(MemberCreateDTO dto) throws BadRequestException;
     MemberDTO updateMember(Integer id, MemberUpdateDTO dto);
     MemberDTO getMemberById(Integer id);
+    Member getMemberEntityById(Integer id);
     void deleteMember(Integer id);
 }
